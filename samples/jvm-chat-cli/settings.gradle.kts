e011e0bd38ef00d25bb1a88fdecfd8e31cfc85f5
Marcos <marcospaulo.s.d@gmail.com>
2026-07-02 23:05:07 -0500

    samples: add jvm chat cli

---
+++ samples/jvm-chat-cli/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "aisdk-jvm-chat-cli-sample"
